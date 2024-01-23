e8663e21fe7fdce1843196e827a40eca91998fab
chrisurf <chrisurf@@users.noreply.github.com>
2024-01-23 17:22:54 +0000

    🚀 initial commit

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Welcome message
+echo -e "\033[1;34mWelcome to the Node.js & TypeScript Development Environment!\033[0m"
+
+# Displaying Node version with [INFO] tag
+echo -e "\033[1;32m[INFO]\033[0m Node version: \033[0;33m$(node -v)\033[0m"
+
+
+# Displaying Yarn version with [INFO] tag
+echo -e "\033[1;32m[INFO]\033[0m Yarn version: \033[0;33m$(yarn -v)\033[0m"
+
