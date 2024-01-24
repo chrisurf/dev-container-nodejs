b818255b307270ddec625088d58a9905f8ef0a33
chrisurf <chrisurf@@users.noreply.github.com>
2024-01-24 16:59:56 +0100

    🌟 cp zshrc

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,12 +7,8 @@ ZSH_CUSTOM=$HOME/.oh-my-zsh
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
-sed -i 's|ZSH_THEME="devcontainers"|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
-echo 'POWERLEVEL9K_MODE="nerdfont-complete"' >> ~/.zshrc
-echo 'ENABLE_CORRECTION="true"' >> ~/.zshrc
 cp /workspace/scripts/config/.p10k.zsh $HOME/.p10k.zsh
-echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
+cp /workspace/scripts/config/.zshrc $HOME/.zshrc
 
 sudo chsh -s /bin/zsh
 sudo sed -i 's|node:x:1000:1000::/home/node:/bin/bash|node:x:1000:1000::/home/node:/bin/zsh|' /etc/passwd
